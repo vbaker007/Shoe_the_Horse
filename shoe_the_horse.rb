d74faa398b74147a0b6c1c6552673db222216a2a
vbaker007 <vbaker007@hotmail.com>
2016-03-21 17:51:11 -0400

    Implement Velocity of Horse

--- shoe_the_horse.rb
+++ shoe_the_horse.rb
@@ -9,9 +9,21 @@ class ShoetheHorse < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
   end
 
   def draw
     @image.draw(@x - @width / 2, @y - @height / 2, 1)
   end
+
+  def update
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= -1 if @x - @width / 2 > 800 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y - @height / 2 > 600 || @y - @height / 2 < 0
+  end 
 end
+
+window = ShoetheHorse.new
+window.show
